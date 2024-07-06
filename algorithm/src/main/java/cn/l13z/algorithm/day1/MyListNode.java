f2b3e714172c30341ea9c27704a0a31e594ef67d
AlfredOrlando <100742520+Outlier-2@users.noreply.github.com>
2024-07-06 12:52:21 +0000

    添加链表

---
+++ algorithm/src/main/java/cn/l13z/algorithm/day1/MyListNode.java
@@ -0,0 +1,58 @@
+public class MyListNode<E> {
+    // 虚拟的头节点与尾节点
+    final private Node<E> head, tail;
+
+    private int size;
+
+    // 双链表节点
+    private static class Node<E> {
+        E val;
+        Node<E> next;
+        Node<E> prev;
+
+        Node(E val) {
+            this.val = val;
+        }
+    }
+
+    public MyListNode() {
+        this.head = new Node<>(null);
+        this.tail = new Node<>(null);
+
+        head.next = tail;
+        tail.prev = head;
+
+        this.size = 0;
+    }
+
+    // 头插法
+    public void addFirst(E e) {
+        Node<E> targetNode = new Node<>(e);
+        Node<E> temp = head.next;
+
+        temp.prev = targetNode;
+        targetNode.next = head;
+        size++;
+    }
+
+    // 删除
+    public E removeFirst() {
+        if (size < 1) {
+            throw new NoSuchElementExecption();
+        }
+
+        Node<E> targetNode = head.next;
+        Node<E> temp = targetNode.next;
+
+        head.next = temp;
+        temp.prev = head;
+
+        targetNode.next = null;
+        targetNode.prev = null;
+
+        size--;
+        return x.val;
+    }
+
+    
+}
