8014edbeb511466d83f38cd4467442a8b4181370
AlfredOrlando <100742520+Outlier-2@users.noreply.github.com>
2024-07-16 13:08:10 +0000

    fix: update

--- designMode/createType/src/main/java/cn/l13z/GeneratorMode/Builder.java
+++ designMode/createType/src/main/java/cn/l13z/GeneratorMode/Builder.java
@@ -12,43 +12,69 @@ import cn.l13z.GeneratorMode.tile.MarcoPoloTile;
  * ClassName: Builder.java <br>
  *
  * @author AlfredOrlando <br>
- * <p>
- * Created: 2024-05-12 02:50 <br> Description: 建造器 <br>
- * <p>
- * Modification History: <br> - 2024/5/12 AlfredOrlando 建造器 <br>
+ *         <p>
+ *         Created: 2024-05-12 02
+ *         50 <br>
+ *         Description: 建造器 <br>
+ *         <p>
+ * 
+ *         Modification History: <br>
+ *         - 2024/5/12 AlfredOrlando 建造器 <br>
  */
 public class Builder {
 
-    public IMenu levelOne(Double area) {
-        return new DecorationPackageMenu(area, "豪华欧式")
-            // 吊顶，二级顶
-            .appendCeiling(new LevelTwoCeiling())
-            // 涂料，多乐士
-            .appendCoat(new DuluxCoat())
-            // 地板，圣象
-            .appendFloor(new ShengXiangFloor());
+    Menu leve rn new DecorationPackageMenu(area,"豪
+    // 吊顶
+    .appendCeiling(new Level
+    // 涂
+    .appendCoat(new DuluxCoat())
+    // 地板，圣象
+    .appendFloor(new ShengXiangFloor());}
+
+    rn new De
+
+    .app
+
+    .appen
+
+    // 地砖，马可波罗
+    .appendTile(new MarcoPoloTile());}
+
+    Menu leve rn new DecorationPackageMenu(area,"现
+    // 吊
+    .appendCeiling(new LevelO
+    // 涂
+    .appendCoat(new LiBangCoat())
+    // 地砖，东鹏
+
+    class Generator {
+
+        /**
+         * Product
+         */
+        public class Product {
+
+            private String part2;
+
+            public void setPart(String part1 )
+            { this.part1 = part;}
+
+            public void setPart2(String part2)
+            {this.part2 = part2;}
+
+
+        }
+
+        /**
+         * Builder
+ 
+         */
+        public interface Builder {
+            void buildPart1();
+            void buildPart2();           
+            Product build();
+            
+        }
     }
 
-    public IMenu levelTwo(Double area) {
-        return new DecorationPackageMenu(area, "轻奢田园")
-
-            // 吊顶，二级顶
-            .appendCeiling(new LevelTwoCeiling())
-
-            // 涂料，立邦
-            .appendCoat(new LiBangCoat())
-
-            // 地砖，马可波罗
-            .appendTile(new MarcoPoloTile());
-    }
-
-    public IMenu levelThree(Double area) {
-        return new DecorationPackageMenu(area, "现代简约")
-            // 吊顶，二级顶
-            .appendCeiling(new LevelOneCeiling())
-            // 涂料，立邦
-            .appendCoat(new LiBangCoat())
-            // 地砖，东鹏
-            .appendTile(new DongPengTile());
-    }
 }
